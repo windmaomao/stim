e24d2b16f19e2a2f409d45d1e7c65e115554c71b
Fang Jin <windmaomao@gmail.com>
2017-08-18 19:18:18 -0400

    setup environment variable for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD_6NUanoEgY8S_aA_FxJRyESrEzkUgUk4",
+    authDomain: "torrid-heat-2030.firebaseapp.com",
+    databaseURL: "https://torrid-heat-2030.firebaseio.com",
+    projectId: "torrid-heat-2030",
+    storageBucket: "torrid-heat-2030.appspot.com",
+    messagingSenderId: "829342570880"
+  }
 };

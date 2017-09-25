d0eddca32dc42b1c0086768f279d65143f326f04
Fang Jin <windmaomao@gmail.com>
2017-09-24 23:05:06 -0400

    refine trade page

--- src/trade/watch.list.page.ts
+++ src/trade/watch.list.page.ts
@@ -19,15 +19,15 @@ export class WatchListPageComponent implements OnInit {
   constructor(private ds: TradeService) {
     this.icons = {
       long: 'md-trending-up green',
-      short: 'md-trending-down lime',
-      tech: 'md-web blue',
-      publication: 'md-my-library-books teal',
-      company: 'md-domain purple',
-      repo: 'md-cloud-queue lime',
+      short: 'md-trending-down',
+      tech: 'md-sd-storage deep-orange',
+      material: 'md-blur-on black',
+      consumer: 'md-store-mall-directory purple',
+      auto: 'md-local-gas-station lime',
       education: 'md-school orange',
-      award: 'md-stars red',
-      blog: 'md-my-library-books light-blue',
-      picture: 'md-image orange',
+      healthcare: 'md-local-hospital light-blue',
+      industry: 'md-local-laundry-service yellow',
+      financial: 'md-local-atm green',
       recommend: 'md-thumb-up green',
       profile: 'md-portrait deep-orange',
     };
@@ -37,10 +37,24 @@ export class WatchListPageComponent implements OnInit {
       this.ds.watches(),
     ).subscribe(([watches]) => {
       watches.forEach(item => {
+        let sgn = (item.type === 'short') ? -1 : 1;
+        if ('price' in item) {
+          if ('target' in item) {
+            item['profit'] = (item.target - item.price) / item.price * 100 * sgn;
+            if (item['profit'] < 0) {
+              item['profit'] = 0.1;
+            }
+          }
+          if ('exit' in item) {
+            item['risk'] = (item.exit - item.price) / item.price * 100 * (-sgn);
+            if (item['risk'] < 0) {
+              item['risk'] = 0.1;
+            }
+          }
+        }
         item['score'] = item.profit / item.risk;
       });
       this.watches = watches;
-      console.log(this.watches);
     });
   }
 }
